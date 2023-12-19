1f5802343703a914710d90abf5df533fc8de6484
joypaul3592 <joypaul3592@gmail.com>
2023-12-19 19:14:43 +0600

    adding new

--- src/app/components/sheard/common/navbar/Logo.jsx
+++ src/app/components/sheard/common/navbar/Logo.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from '../../../../assets/image/logo/logowhite.png'
+import logo from '../../../../assets/image/logo/logoWhite.png'
 
 export default function Logo() {
     return (
